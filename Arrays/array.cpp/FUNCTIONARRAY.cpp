18d73ba42c9f89f2fe9f64bceb6c5f08f1fb14a5
Kulshrest Parjapati <158595134+Kulshrset@users.noreply.github.com>
2026-01-08 07:41:15 +0530

    Add files via upload

---
+++ Arrays/array.cpp/FUNCTIONARRAY.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void print(int arr[],int size){
+
+}
+
+int main(){
+int arr[]={10,20,30,40,50};
+int size=5;
+print(arr, size);
+
+
+
+}
---
+++ Arrays/arrays.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void solve1()
+int main(){
+//int arr[10]; 
+
+//int brr[]={10,20,5,6,9};
+//cout<<brr[4]<<endl;
+
+/*int arr[4];
+fill(arr,arr+4,101);
+cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2]
+<<" "<<arr[3]<<endl;*/
+
+
+return 0;
+}
