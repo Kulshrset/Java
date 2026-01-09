af98e66a485bb9104ec5c96ba1064ac43d31ba69
Kulshrest Parjapati <158595134+Kulshrset@users.noreply.github.com>
2026-01-09 15:22:19 +0530

    Add files via upload

---
+++ loops/for.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+ for(int count=0; count<=10; count=count+1){
+    cout<<"kulshrest"<<endl;
+ }
+
+ for(int i=0;i<=5;i=i+1){
+    cout<<i<<" ";
+ }
+ for(int i=0;i<=5;i=i+1){
+    cout<<i<<" ";
+ }
+
+return 0;
+}
