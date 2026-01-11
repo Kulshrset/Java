51b83a297fad098471500456e99dd106602517a7
Kulshrest Parjapati <158595134+Kulshrset@users.noreply.github.com>
2026-01-11 17:30:28 +0530

    Add files via upload

---
+++ Decision making/if.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main(){
+int budget;
+
+//cout<< "enter your budget"<<endl;
+
+//input
+
+
+//cin>> budget;
+/*if(budget>200000){
+    cout<<"you can buy scorpio";
+}
+else{
+    cout<<"you can not byu anything";
+}*/
+int marks;
+cout<<"enter your marks\n";
+
+cin>>marks;
+
+if(marks>90){
+    cout<<"you r passed\n";
+
+    cout<<"A";
+}
+else if(marks>=70){
+    cout<<"you r passed\n";
+
+    cout<<"B";
+}
+else if(marks>=50){
+    cout<<"you r in mid\n";
+
+    cout<<"C";
+}
+else if(marks<45){
+    cout<<"you r fail38
+    \n";
+
+    cout<<"D";
+}
+return 0;
+}
---
+++ Decision making/ternery operators/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"enter age\n";
+    cin>>age;
+   (age >18) ? cout<<"you can vote\n"
+   :cout<<"can not vote";
+
+    return 0;
+}
