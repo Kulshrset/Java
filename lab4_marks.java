0168446cd4262dff59d502d09c1df198b2a9611b
Kulshrest Parjapati <158595134+Kulshrset@users.noreply.github.com>
2026-01-19 21:59:08 +0530

    Add files via upload

---
+++ lab4_marks.java
@@ -0,0 +1,20 @@
+package ex_15_Arrays;
+
+public class lab4_marks {
+    static void main(String[] args) {
+       // int [] marks= new int[10];
+        int [] marks1={40,54,85,68,52,54};
+        System.out.println(marks1[0]);
+        System.out.println(marks1[1]);
+        System.out.println(marks1[2]);
+        System.out.println(marks1[3]);
+
+
+
+        for(int i=0;i< marks1.length;i++){
+            System.out.println(marks1[i]);
+        }
+
+
+    }
+}
